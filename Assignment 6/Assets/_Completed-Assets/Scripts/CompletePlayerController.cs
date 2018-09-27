8204bfab802126930151b4fb327f95ecfeadcebc
Blair Crutchfield <bc18b@my.fsu.edu>
2018-09-27 15:24:14 -0400

    Assignment 6 Final

--- Assignment 6/Assets/_Completed-Assets/Scripts/CompletePlayerController.cs
+++ Assignment 6/Assets/_Completed-Assets/Scripts/CompletePlayerController.cs
@@ -4,73 +4,81 @@ using System.Collections;
 //Adding this allows us to access members of the UI namespace including Text.
 using UnityEngine.UI;
 
-public class CompletePlayerController : MonoBehaviour {
+public class CompletePlayerController : MonoBehaviour
+{
 
     public float speed;             //Floating point variable to store the player's movement speed.
-    public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
-    public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
+    public Text MeteorcountText;          //Store a reference to the UI Text component which will display the number of pickups collected.
+    public Text StarText;   //Star Points' message.
 
     private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-    private int count;              //Integer to store the number of pickups collected so far.
-
+    private int Meteorcount;              //Integer to store the number of pickups collected so far.
+    private int Starcount;
     // Use this for initialization
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D> ();
+        rb2d = GetComponent<Rigidbody2D>();
 
         //Initialize count to zero.
-        count = 0;
+        Meteorcount = 0;
 
-        //Initialze winText to a blank string since we haven't won yet at beginning.
-        winText.text = "";
+        //Initialize count to zero.
+        Starcount = 0;
+
+       
+       
 
         //Call our SetCountText function which will update the text with the current value for count.
-        SetCountText ();
+        SetCountText();
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
         //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
         //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveVertical = Input.GetAxis("Vertical");
 
         //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce (movement * speed);
+        rb2d.AddForce(movement * speed);
     }
 
     //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
-    void OnTriggerEnter2D(Collider2D other) 
+    void OnTriggerEnter2D(Collider2D other)
     {
         //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-        if (other.gameObject.CompareTag ("PickUp")) 
+        if (other.gameObject.CompareTag("PickUp"))
         {
             //... then set the other object we just collided with to inactive.
             other.gameObject.SetActive(false);
-            
+
             //Add one to the current value of our count variable.
-            count = count + 1;
-            
+            Meteorcount = Meteorcount + 1;
+
             //Update the currently displayed count by calling the SetCountText function.
-            SetCountText ();
+            SetCountText();
+
+
         }
         //Add the other pickup for a point value of 2
-        if (other.gameObject.CompareTag ("PickUp2"))
+        if (other.gameObject.CompareTag("PickUp2"))
         {
             //set it to inactive
             other.gameObject.SetActive(false);
 
             //Add more points to count
-            count = count + 2;
+            Starcount = Starcount + 1;
 
             //Update again for new object
-            SetCountText ();
+            SetCountText();
+
+
         }
 
     }
@@ -79,11 +87,12 @@ public class CompletePlayerController : MonoBehaviour {
     void SetCountText()
     {
         //Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
-        countText.text = "Count: " + count.ToString ();
+        MeteorcountText.text = "MeteorCount: " + Meteorcount.ToString();
 
-        //Check if we've collected all 12 pickups. If we have...
-        if (count >= 13)
-            //... then set the text property of our winText object to "You win!"
-            winText.text = "You win!";
+        //Set Text property for the other one
+        StarText.text = "StarCount: " + Starcount.ToString();
     }
+
+
+
 }
